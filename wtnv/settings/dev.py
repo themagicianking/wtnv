6d619ff378f7c56771dc1a75cf576673662ae5f7
Thane Chaves <tmoonder@gmail.com>
2025-01-06 14:00:05 -0500

    Wagtail setup.

---
+++ wtnv/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-=r2z3wy68ht3ddc&n$=fvm9%=zs(l45i1=qrk&2arznw7opy@d"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

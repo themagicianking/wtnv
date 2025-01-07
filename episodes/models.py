871e488b030f43017e95780a3c0eccbe090162c4
Thane Chaves <tmoonder@gmail.com>
2025-01-06 23:29:54 -0500

    Created a text file to store rss feed.

--- episodes/models.py
+++ episodes/models.py
@@ -1,9 +1,24 @@
 from django.db import models
-
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+import os
+import feedparser
+import re
 
+rss_data = "/Users/tpl1122_1/Desktop/projects/wtnv/episodes/rss-feed.txt"
+
+with open(rss_data, 'r') as file_obj:
+    first_char = file_obj.read(1)
+
+    if not first_char:
+        wtnv_rss_url = "https://feeds.nightvalepresents.com/welcometonightvalepodcast"
+        feed = feedparser.parse(wtnv_rss_url)
+        file = open(rss_data, "a")
+        file.write(str(feed))
+        file.close()
+    else:
+        print("text file created already.")
 
 class EpisodeIndexPage(Page):
     intro = RichTextField(blank=True)
